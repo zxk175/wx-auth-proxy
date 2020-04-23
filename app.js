4ea1fbb80c87a5dae2fee2e5bccb9c6c96c4e4bc
zxk175 <zxk175@qq.com>
2020-04-23 22:17:30 +0800

    initial commit

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ app.use(logger())
 app.use(statics(__dirname + '/public'))
 app.use(views(__dirname + '/views', {extension: 'ejs'}))
 
-// logger
-app.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
 // routes
 app.use(router.routes());
 
